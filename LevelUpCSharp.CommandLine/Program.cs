12dceb47115af7c0ef92bd7b80b9ef9fe226871a
Piotr Barankiewicz <pbarank@ra.rockwell.com>
2021-02-08 10:48:27 +0100

    extensions sample

--- LevelUpCSharp.CommandLine/Program.cs
+++ LevelUpCSharp.CommandLine/Program.cs
@@ -6,7 +6,39 @@ namespace LevelUpCSharp.CommandLine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int myNumber = 5;
+
+            int mySecondNuymbner = 4;
+
+            if (myNumber % 2 == 1)
+            {
+                /* react on odd */
+            }
+
+            if (MyExtenstion.IsOddOldWay(myNumber))
+            {
+                /* react on odd */
+            }
+
+            if (myNumber.IsOdd())
+            {
+                /* react on odd */
+            }
+
+            mySecondNuymbner.IsOdd();
+        }
+    }
+
+    static class MyExtenstion
+    {
+        public static bool IsOdd(this int source)
+        {
+            return source % 2 == 1;
+        }
+
+        public static bool IsOddOldWay(int source)
+        {
+            return source % 2 == 1;
         }
     }
 }
